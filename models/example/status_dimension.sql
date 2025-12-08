8f20b5e5b6f2ae3a5c9839e692d24a13ae11e9e4
joshgindis <121984011+joshgindis@users.noreply.github.com>
2025-12-08 12:26:10 -0500

    Create status_dimension.sql

---
+++ models/example/status_dimension.sql
@@ -0,0 +1,13 @@
+WITH status_columns AS (
+    SELECT DISTINCT
+        current_status_ID AS Current_Status_ID
+        current_status AS Current_Status
+        current_status_date AS Current_Status_Date
+    FROM {{ ref('raw_housing_code_violations') }}
+)
+
+SELECT
+    row_number() OVER() AS Status_Dim_ID,
+    *,
+    current_timestamp() AS loaded_at
+FROM status_columns

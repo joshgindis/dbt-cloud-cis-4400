49298b26ca2ff0e8864d317055b90ef4e7213cc6
joshgindis <121984011+joshgindis@users.noreply.github.com>
2025-12-08 13:25:44 -0500

    Create notice_of_violation_dimension.sql

---
+++ models/example/notice_of_violation_dimension.sql
@@ -0,0 +1,12 @@
+WITH location_columns AS (
+    SELECT DISTINCT
+        notice_of_violation_type AS Notice_of_Violation_Type,
+        notice_of_violation_description AS Notice_of_Violation_Description
+    FROM {{ ref('raw_housing_code_violations') }}
+)
+
+SELECT
+    row_number() OVER() AS Notice_of_Violation_Dim_ID,
+    *,
+    current_timestamp() AS loaded_at
+FROM notice_of_violation_columns

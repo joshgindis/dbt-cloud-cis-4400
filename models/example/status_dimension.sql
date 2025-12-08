abf33cc242779598eeb90b482b217d62da5f6bc0
joshgindis <121984011+joshgindis@users.noreply.github.com>
2025-12-08 13:26:04 -0500

    Update status_dimension.sql

--- models/example/status_dimension.sql
+++ models/example/status_dimension.sql
@@ -1,7 +1,7 @@
 WITH status_columns AS (
     SELECT DISTINCT
-        current_status_ID AS Current_Status_ID
-        current_status AS Current_Status
+        current_status_ID AS Current_Status_ID,
+        current_status AS Current_Status,
         current_status_date AS Current_Status_Date
     FROM {{ ref('raw_housing_code_violations') }}
 )

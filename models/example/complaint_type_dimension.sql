4a5a449c7b52bf72d683730ea6023faa14d8ad84
JagmeetMinhas <69373358+JagmeetMinhas@users.noreply.github.com>
2025-12-08 18:06:28 -0500

    Create complaint_type_dimension.sql

---
+++ models/example/complaint_type_dimension.sql
@@ -0,0 +1,12 @@
+WITH complaint_type_columns AS (
+    SELECT DISTINCT
+        complainttype AS Complaint_Type,
+        complaintdescription AS Complaint_Description
+    FROM {{ ref('raw_311_complaints') }}
+)
+
+SELECT
+    row_number() OVER() AS Complaint_Type_Dim_ID,
+    *,
+    current_timestamp() AS loaded_at
+FROM complaint_type_columns

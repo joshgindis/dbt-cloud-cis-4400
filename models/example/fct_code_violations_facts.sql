f5b666e20fb4ac651dfe668879d0f10b7385dc32
JagmeetMinhas <69373358+JagmeetMinhas@users.noreply.github.com>
2025-12-19 15:52:03 -0500

    Update fct_code_violations_facts.sql

--- models/example/fct_code_violations_facts.sql
+++ models/example/fct_code_violations_facts.sql
@@ -54,10 +54,10 @@ INNER JOIN building_dimension bd
 
 INNER JOIN location_dimension ld
     ON v.boro = ld.Borough
-    AND v.streetname = ld.Street_Name,
-    AND v.apartment = ld.Apartment,
-    AND v.cross_street_2 = ld.Cross_Street_2,
-    AND v.councildistrict = ld.Council_District,
+    AND v.streetname = ld.Street_Name
+    AND v.apartment = ld.Apartment
+    AND v.cross_street_2 = ld.Cross_Street_2
+    AND v.councildistrict = ld.Council_District
     AND v.censustract = ld.Census_Tract
 
 INNER JOIN status_dimension sd

ea30940d78602a08a557cd084d417f3b9ccbb3f4
JagmeetMinhas <69373358+JagmeetMinhas@users.noreply.github.com>
2025-12-19 14:33:24 -0500

    Update notice_of_violation_dimension.sql

--- models/example/notice_of_violation_dimension.sql
+++ models/example/notice_of_violation_dimension.sql
@@ -2,7 +2,7 @@ WITH notice_of_violation_columns AS (
     SELECT DISTINCT
         novtype AS Notice_of_Violation_Type,
         novdescription AS Notice_of_Violation_Description,
-        novissuedate AS Notice_of_Violation_Issue_Date
+        novissueddate AS Notice_of_Violation_Issue_Date
     FROM {{ ref('raw_housing_code_violations') }}
 )
 
